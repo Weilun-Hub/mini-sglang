160c4a6a9f2e1638670cce5c6001f339b8717aed
赵威伦 <zhaoweilun@zhaoweiluns-MacBook-Pro.local>
2026-01-05 16:49:34 +0800

    update launch.sh

--- tests/spec/launch.sh
+++ tests/spec/launch.sh
@@ -1,11 +1,12 @@
 
 export CUDA_VISIBLE_DEVICES="6,7"
 
-TARGET_PATH=""
-DRAFT_PATH=""
+TARGET_PATH="/media/disk1/models/Qwen3-14B"
+DRAFT_PATH="/home/relay/liujiacheng06/models/Qwen3-0.6B"
 
 python3 -m minisgl \
     --target-model-path ${TARGET_PATH} \
     --draft-model-path ${DRAFT_PATH} \
     --target-tensor-parallel-size 1 \
     --draft-tensor-parallel-size 1 \
+    --port 1996 \
