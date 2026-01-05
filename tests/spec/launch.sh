11bfa250c7138dba8ae33a3003562badd3cdb54c
赵威伦 <zhaoweilun@zhaoweiluns-MacBook-Pro.local>
2026-01-05 15:06:52 +0800

    add test

---
+++ tests/spec/launch.sh
@@ -0,0 +1,11 @@
+
+export CUDA_VISIBLE_DEVICES="6,7"
+
+TARGET_PATH=""
+DRAFT_PATH=""
+
+python -m minisgl \
+    --target-model-path ${TARGET_PATH} \
+    --draft-model-path ${DRAFT_PATH} \
+    --target-tensor-parallel-size 1 \
+    --draft-tensor-parallel-size 1 \
