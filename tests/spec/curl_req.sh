ba326b1023ef51611712317d49be4fa7b0144e5f
赵威伦 <zhaoweilun@zhaoweiluns-MacBook-Pro.local>
2026-01-07 12:18:24 +0800

    fix bug

--- tests/spec/curl_req.sh
+++ tests/spec/curl_req.sh
@@ -1,6 +1,7 @@
 curl -X POST "http://localhost:1996/v1/chat/completions" \
   -H "Content-Type: application/json" \
   -d '{
+    "model": "",
     "messages": [{"role":"user","content":"Hello, how are you?"}],
     "max_tokens": 2
   }'
