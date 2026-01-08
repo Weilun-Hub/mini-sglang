87ca411026125b19f79c9f65cd6e90ae1d59be14
赵威伦 <zhaoweilun@zhaoweiluns-MacBook-Pro.local>
2026-01-08 14:40:10 +0800

    add log

--- python/minisgl/scheduler/scheduler.py
+++ python/minisgl/scheduler/scheduler.py
@@ -351,8 +351,10 @@ class DraftScheduler(Scheduler):
             req.append_host(next_token_id.unsqueeze(0))
             next_token = int(next_token_id.item())
             finished = req.remain_len <= 0
+            logger.info(f"{torch.distributed.get_rank()} finish point 0, finished: {finished}")
             if not req.sampling_params.ignore_eos:
                 finished |= next_token == self.eos_token_id
+            logger.info(f"{torch.distributed.get_rank()} finish point 1, finished: {finished}")
             if req.device_len >= max_seq_len - 1 + self.gamma:
                 finished = True
                 logger.warning_rank0(f"Request {req.uid} reached {max_seq_len = }, dropped.")
