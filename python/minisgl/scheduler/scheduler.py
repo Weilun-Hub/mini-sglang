baa12aacc266b186d19eefeb4f8ed0a7fa41e2c3
赵威伦 <zhaoweilun@zhaoweiluns-MacBook-Pro.local>
2026-01-09 11:25:35 +0800

    fix bug

--- python/minisgl/scheduler/scheduler.py
+++ python/minisgl/scheduler/scheduler.py
@@ -325,6 +325,19 @@ class TargetScheduler(Scheduler):
         logger.info(f'{rank} Logits shape: {logits.shape}')
         logger.info(f'{rank} Sample args: {sample_args}')
 
+        if local_rank == 0:
+            
+            num_to_be_verified_tokens = sum([1 if req.pre_verify else self.gamma for req in reqs])
+            num_next_round_input = self.gamma * len(reqs)
+            msg = torch.zeros(num_to_be_verified_tokens + num_next_round_input, dtype=torch.int64, device="cuda")
+            src_rank = self.tp_info.world_size - self.tp_info.local_size # draft rank 0
+            torch.distributed.broadcast(msg, src=src_rank, group=self.engine.verify_group)
+            to_be_verified_tokens = msg[:num_to_be_verified_tokens].cpu().numpy().tolist()
+            next_round_input = msg[num_to_be_verified_tokens:].cpu().numpy().tolist()
+
+            logger.info(f"{torch.distributed.get_rank()} Received to_be_verified_tokens: {to_be_verified_tokens}")
+            logger.info(f"{torch.distributed.get_rank()} Received next_round_input: {next_round_input}")
+
 
 class DraftScheduler(Scheduler):
     def __init__(self, config: SchedulerConfig):
