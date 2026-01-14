198fc8755456ddf294a65235a8a1109406cf2c3f
赵威伦 <zhaoweilun@zhaoweiluns-MacBook-Pro.local>
2026-01-14 21:12:10 +0800

    8 step test

--- python/minisgl/scheduler/scheduler.py
+++ python/minisgl/scheduler/scheduler.py
@@ -290,7 +290,7 @@ class Scheduler(SchedulerIOMixin):
                 # while True:
                 #     self.normal_loop()
 
-                for i in range(8):
+                for i in range(9):
                     logger.info(f"{torch.distributed.get_rank()} ========================= step {i} =========================")
                     logger.info(f"{torch.distributed.get_rank()} ========================= step {i} =========================")
                     logger.info(f"{torch.distributed.get_rank()} ========================= step {i} =========================")
