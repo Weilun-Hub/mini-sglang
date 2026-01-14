c6d886a4e20f132d0a4d8826dac0774e948f3571
赵威伦 <zhaoweilun@zhaoweiluns-MacBook-Pro.local>
2026-01-14 15:07:15 +0800

    fix bug

--- python/minisgl/scheduler/scheduler.py
+++ python/minisgl/scheduler/scheduler.py
@@ -470,7 +470,7 @@ class TargetScheduler(Scheduler):
                 r = torch.rand(num_to_be_verified_tokens, device="cuda")
                 target_logits = torch.zeros(logits.shape, device=logits.device, dtype=logits.dtype)
                 for i in range(logits.shape[0]):
-                    target_logits[i] = sampling.softmax(logits[i], sample_args.temperatures, enable_pdl=is_sm90_supported())
+                    target_logits[i : i + 1] = sampling.softmax(logits[i : i + 1], sample_args.temperatures, enable_pdl=is_sm90_supported())
                 # target_logits = sampling.softmax(logits, sample_args.temperatures, enable_pdl=is_sm90_supported())
                 target_prob = target_logits.gather(dim=1, index=msg[:num_to_be_verified_tokens].unsqueeze(1)).squeeze(1)
                 judge = (r <= target_prob).tolist()
