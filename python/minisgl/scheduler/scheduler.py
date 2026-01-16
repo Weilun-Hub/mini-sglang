f9b7b3f2c9f3b71e7680ac9dbe7d1c37bd215489
赵威伦 <zhaoweilun@zhaoweiluns-MacBook-Pro.local>
2026-01-16 18:39:50 +0800

    fix bug

--- python/minisgl/scheduler/scheduler.py
+++ python/minisgl/scheduler/scheduler.py
@@ -470,11 +470,9 @@ class TargetScheduler(Scheduler):
                     if acc[idx]:
                         req.pre_verify = False
                         _tokens = torch.as_tensor(next_round_input[self.gamma * idx : self.gamma * (idx + 1)], dtype=self.token_pool.dtype, device=self.token_pool.device)
-                        try:
-                            self.token_pool.view(-1)[forward_input.write_indices : forward_input.write_indices + self.gamma] = _tokens
-                        except Exception as e:
-                            print("[DEBUG]",forward_input.write_indices, forward_input.write_indices.dtype)
-                            raise RuntimeError
+
+                        print("[DEBUG] forward_input.write_indices",forward_input.write_indices, forward_input.write_indices.dtype)
+                        self.token_pool.view(-1)[forward_input.write_indices : forward_input.write_indices + self.gamma] = _tokens
                         
                         req.append_host(torch.tensor(next_round_input[self.gamma * idx : self.gamma * (idx + 1)]))
                         req.device_len += self.gamma
