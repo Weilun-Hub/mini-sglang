1bdde5280af605341b5536ae1d0164fe7d5a3a36
赵威伦 <zhaoweilun@zhaoweiluns-MacBook-Pro.local>
2026-01-17 00:36:02 +0800

    add log

--- python/minisgl/scheduler/scheduler.py
+++ python/minisgl/scheduler/scheduler.py
@@ -406,7 +406,7 @@ class TargetScheduler(Scheduler):
 
                 logger.info(f"{torch.distributed.get_rank()} to_be_verified_tokens {to_be_verified_tokens}")
                 logger.info(f"{torch.distributed.get_rank()} next_round_input {next_round_input}")
-                logger.info(f"{torch.distributed.get_rank()} logits.shape {target_logits.shape}")
+                logger.info(f"{torch.distributed.get_rank()} logits.shape {logits.shape}")
 
                 r = torch.rand(num_to_be_verified_tokens, device="cuda")
                 
