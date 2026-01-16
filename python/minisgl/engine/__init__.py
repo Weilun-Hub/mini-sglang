643556aa282fef07a9fd106bfaea363101a059ad
赵威伦 <zhaoweilun@zhaoweiluns-MacBook-Pro.local>
2026-01-16 13:45:26 +0800

    fix bug

--- python/minisgl/engine/__init__.py
+++ python/minisgl/engine/__init__.py
@@ -1,5 +1,5 @@
 from .config import EngineConfig
-from .engine import Engine, DraftEngine, TargetEngine, ForwardOutput
+from .engine import Engine, DraftEngine, TargetEngine, ForwardOutput, VerifyOutput
 from .sample import BatchSamplingArgs
 
-__all__ = ["Engine", "DraftEngine", "TargetEngine", "EngineConfig", "ForwardOutput", "BatchSamplingArgs"]
+__all__ = ["Engine", "DraftEngine", "TargetEngine", "EngineConfig", "ForwardOutput", "VerifyOutput", "BatchSamplingArgs"]
