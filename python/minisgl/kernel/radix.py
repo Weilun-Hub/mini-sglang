b31b15db22cd3a7592ba6a63021a721ae6b4ebb4
赵威伦 <zhaoweilun@zhaoweiluns-MacBook-Pro.local>
2026-01-16 18:24:28 +0800

    fix bug

--- python/minisgl/kernel/radix.py
+++ python/minisgl/kernel/radix.py
@@ -20,11 +20,10 @@ def fast_compare_key(x: torch.Tensor, y: torch.Tensor) -> int:
     # compare 2 1-D int cpu tensors for equality
 
     print(
-        "[fast_compare_key] x:",
+        f"{torch.distributed.get_rank()} [fast_compare_key] x:",
         x.shape, x.dtype, x.device,
         "| y:",
         y.shape, y.dtype, y.device,
-        flush=True,
     )
      
     assert x.dim() == 1 and y.dim() == 1, "x/y must be 1-D"
